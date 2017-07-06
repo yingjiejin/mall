177b40199085d8a3d4a9d7ead45059a411f81294
jinyingjie <13520460664@163.com>
2017-07-06 17:17:06 +0800

    品类接口

--- src/main/java/com/mall/controller/backend/CategoryManageController.java
+++ src/main/java/com/mall/controller/backend/CategoryManageController.java
@@ -34,7 +34,7 @@ public class CategoryManageController {
      * @param parentId
      * @return
      */
-    @RequestMapping("add_Category.do")
+    @RequestMapping("add_category.do")
     @ResponseBody
     public ServerResponse addCategory(HttpSession session, String categoryName, @RequestParam(value = "parentId", defaultValue = "0") int parentId) {
         User user = (User) session.getAttribute(Const.CURRENT_USER);
@@ -48,8 +48,75 @@ public class CategoryManageController {
         } else {
             return ServerResponse.createByErrorMessage("无权限操作,需要管理员权限");
         }
+    }
 
+    /**
+     * 更新品类名称
+     *
+     * @param session
+     * @param categoryId
+     * @param categoryName
+     * @return
+     */
+    @RequestMapping("set_category_name.do")
+    @ResponseBody
+    public ServerResponse setCategoryName(HttpSession session, Integer categoryId, String categoryName) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录,请登录");
+        }
+        if (iUserService.checkAdminRole(user).isSuccess()) {
+            //是管理员,更新categoryName
+            return iCategoryService.updateCategoryName(categoryId, categoryName);
+        } else {
+            return ServerResponse.createByErrorMessage("无权限操作,需要管理员权限");
+        }
+    }
+
+    /**
+     * 根据categoryId获取当前categoryId子节点的信息
+     *
+     * @param session
+     * @param categoryId
+     * @return
+     */
+    @RequestMapping("get_category.do")
+    @ResponseBody
+    public ServerResponse getChildrenParallelCategory(HttpSession session, @RequestParam(value = "categoryId", defaultValue = "0") Integer categoryId) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录,请登录");
+        }
+        if (iUserService.checkAdminRole(user).isSuccess()) {
+            //查询Category的子节点信息,并且不递归保持平级
+            return iCategoryService.getChildParallelCategory(categoryId);
+        } else {
+            return ServerResponse.createByErrorMessage("无权限操作,需要管理员权限");
+        }
+    }
+
+    /**
+     * 递归查询本节点的id及孩子节点的id
+     *
+     * @param session
+     * @param categoryId
+     * @return
+     */
+    @RequestMapping("get_deep_category.do")
+    @ResponseBody
+    public ServerResponse getCategoryAndDeepChildCategory(HttpSession session, @RequestParam(value = "categoryId", defaultValue = "0") Integer categoryId) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录,请登录");
+        }
+        if (iUserService.checkAdminRole(user).isSuccess()) {
+            //查询当前节点的id和递归子节点的id
+            return iCategoryService.selectCategoryAndChildrenById(categoryId);
+        } else {
+            return ServerResponse.createByErrorMessage("无权限操作,需要管理员权限");
+        }
 
     }
+
 
 }
--- src/main/java/com/mall/service/ICategoryService.java
+++ src/main/java/com/mall/service/ICategoryService.java
@@ -1,6 +1,9 @@
 package com.mall.service;
 
 import com.mall.common.ServerResponse;
+import com.mall.pojo.Category;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/7/4.
@@ -8,4 +11,10 @@ import com.mall.common.ServerResponse;
 public interface ICategoryService {
 
     ServerResponse addCategory(String categoryName, Integer parentId);
+
+    ServerResponse updateCategoryName(Integer categoryId, String categoryName);
+
+    ServerResponse<List<Category>> getChildParallelCategory(Integer categoryId);
+
+    ServerResponse selectCategoryAndChildrenById(Integer categoryId);
 }
